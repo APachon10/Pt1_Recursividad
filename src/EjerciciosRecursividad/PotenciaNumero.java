1476b8d63924663a5334031a211edae234394723
APachon10 <Pachondestroy95@gmail.com>
2018-12-17 12:09:48 +0100

    Clase potenciaNumero ( Ej 7 ) Creada

---
+++ src/EjerciciosRecursividad/PotenciaNumero.java
@@ -0,0 +1,8 @@
+package EjerciciosRecursividad;
+
+public class PotenciaNumero {
+	public static void main(String[] args) {
+		//Creamos el Objeto con el que llamaremos a la variable
+		PotenciaNumero pv = new PotenciaNumero();
+	}
+}

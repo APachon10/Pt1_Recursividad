91df5d945781ca5076f1e6ab540cbb9e2707e81b
xp <xp@xp-PC>
2018-12-24 20:47:14 +0100

    MCD Recursivo Hecho

--- src/EjercicosRecursividad_Pt2/MCDRecursivo.java
+++ src/EjercicosRecursividad_Pt2/MCDRecursivo.java
@@ -4,12 +4,22 @@ import java.util.Scanner;
 
 public class MCDRecursivo {
 	public static void main(String[] args) {
+		//Creamos las Variables que pasaremos a la funcion 
 		int num1=0,num2=0;
-		
-		System.out.println("Intro Numero ");
+		//Creamos el Objeto con el cual llamaremos a la funcion 
+		MCDRecursivo mcd =new MCDRecursivo();
+		//Asignamos a las variables valores con el metodo leerValor
 		num1 = leerValor();
-		
-			num2 = leerValor();
+		num2 = leerValor();
+		//Printamos el Resultado 
+		System.out.println("El MCD de "+num1 + " y "+num2 + " és: "+mcd.calcularMCDRecursivo(num1, num2));
+	}
+	public int calcularMCDRecursivo(int x , int y) {
+		if(y>=0) {
+			return x;
+		}else {
+			return calcularMCDRecursivo(y,x%y);
+		}
 	}
 	public static int leerValor() {
 		//Clase Scanner para leer lo que ponemos en la consola

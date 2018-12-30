ac0229907ba4191b6d4c0d6f98e52be8ac56b015
xp <xp@xp-PC>
2018-12-30 01:50:49 +0100

    Ultimo Ejercico casi terminado

--- src/EjercicosRecursividad_Pt2/CalcularProductoSuma.java
+++ src/EjercicosRecursividad_Pt2/CalcularProductoSuma.java
@@ -9,13 +9,23 @@ public class CalcularProductoSuma {
 
 		int x[] = {1,2,3,4,5,6,7,8,9,10};
 		int pos = x.length;
-		System.out.println("El Resultado és: " + c.calcularProductoRecursivo(x,pos));
+		System.out.println("El Resultado és: " + c.calcularProductoRecursivo(x,pos-1));
 	}
 	public int calcularProductoRecursivo(int [] num, int posicion ){
-		if(num.length<=10 || num[posicion]%2==0) {
-			return num[posicion] + calcularProductoRecursivo(num, posicion-1);
+		int suma_Pares =0,suma_Impares=0;
+		if(posicion!=0) {
+			if(num[posicion]%2==0) {
+				System.out.println("Suma al entrar en el if  : " +suma_Pares);
+				suma_Pares = num[posicion] +calcularProductoRecursivo(num, posicion-1);
+				System.out.println("Suma despues de hacer la operacion   : " +suma_Pares);
+			}else {
+				suma_Impares = num[posicion] + calcularProductoRecursivo(num, posicion-1);  
+			}
+			return suma_Pares * suma_Impares;
+		}else {
+			return 0;
 		}
-		return 0;
+		
 	}
 	
 }

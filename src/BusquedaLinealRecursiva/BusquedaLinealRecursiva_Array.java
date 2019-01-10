a505c5d37d86dc56f071ca1a2db8e7964a2a617d
APachon10 <Pachondestroy95@gmail.com>
2019-01-10 08:17:46 +0100

    Ejercicoi Busqueda lineal recursiva creado , no terminado

---
+++ src/BusquedaLinealRecursiva/BusquedaLinealRecursiva_Array.java
@@ -0,0 +1,8 @@
+package BusquedaLinealRecursiva;
+
+public class BusquedaLinealRecursiva_Array {
+	public static void main(String[] args) {
+		BusquedaLinealRecursiva_Array busqueda = new BusquedaLinealRecursiva_Array();
+		int numeros [] = {1,5,3,10,2,6,7,8,9,4};
+	}
+}

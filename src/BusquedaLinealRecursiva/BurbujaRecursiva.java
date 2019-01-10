9f4a5d0ad743c00c4a59d7ed951f0f336ca764a1
APachon10 <Pachondestroy95@gmail.com>
2019-01-10 09:41:16 +0100

    v

--- src/BusquedaLinealRecursiva/BurbujaRecursiva.java
+++ src/BusquedaLinealRecursiva/BurbujaRecursiva.java
@@ -23,12 +23,9 @@ public class BurbujaRecursiva {
 			}
 		}
 		return v;
-////		 for(int i = 0; i < arreglo.length - 1; i++)
-//        {
-//            for(int j = 0; j < arreglo.length - 1; j++)
-//            {
-//                if (arreglo[j] < arreglo[j + 1])
-//                {
+//		 for(int i = 0; i < arreglo.length - 1; i++){
+//            for(int j = 0; j < arreglo.length - 1; j++){
+//                if (arreglo[j] < arreglo[j + 1]){
 //                    int tmp = arreglo[j+1];
 //                    arreglo[j+1] = arreglo[j];
 //                    arreglo[j] = tmp;

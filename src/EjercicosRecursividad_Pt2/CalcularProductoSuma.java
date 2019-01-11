ee909a05d660ad39c5e153c1c37cb7f30d7afea8
APachon10 <Pachondestroy95@gmail.com>
2019-01-11 12:01:28 +0100

    CalcularProductoSuma no hecho

--- src/EjercicosRecursividad_Pt2/CalcularProductoSuma.java
+++ src/EjercicosRecursividad_Pt2/CalcularProductoSuma.java
@@ -1,31 +1,47 @@
 package EjercicosRecursividad_Pt2;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class CalcularProductoSuma {
 	public static void main(String[] args) {
-		
+
 		CalcularProductoSuma c = new CalcularProductoSuma();
 
-		int x[] = {1,2,3,4,5,6,7,8,9,10};
-		int pos = x.length;
+		int x[] = new int [10];
+		c.rellenarVector(x);
+		c.mostrarVector(x);
+		
+		System.out.println("==============");
+		int pos= x.length;
+		
 		System.out.println("El Resultado és: " + c.calcularProductoRecursivo(x,pos-1));
 	}
 	public int calcularProductoRecursivo(int [] num, int posicion ){
-		int suma_Pares =0,suma_Impares=0;
-		if(posicion!=0) {
-			if(num[posicion]%2==0) {
-				System.out.println("Suma al entrar en el if  : " +suma_Pares);
-				suma_Pares = num[posicion] +calcularProductoRecursivo(num, posicion-1);
-				System.out.println("Suma despues de hacer la operacion   : " +suma_Pares);
-			}else {
-				suma_Impares = num[posicion] + calcularProductoRecursivo(num, posicion-1);  
-			}
-			return suma_Pares * suma_Impares;
-		}else {
+		if (posicion==0) {
 			return 0;
+		}	
+		if (num[posicion]%2==0) {
+			System.out.println("multiplo de 2:" + num[posicion]);
+			return num[posicion]+calcularProductoRecursivo(num, posicion-1);
+		}else if(num[posicion]%3==0) {
+			System.out.println("multiplo de 3:" + num[posicion]);
+			return num[posicion]+calcularProductoRecursivo(num, posicion-1);
+		}else if(num[posicion]%3!= 0 && num[posicion]%2!=0 ) {
+			System.out.println("no soy multiplo de nadie:" + num[posicion]);
+			calcularProductoRecursivo(num, posicion-1);
+			return num[posicion];
 		}
-		
+		return num[posicion];
 	}
-	
+	public  int [] rellenarVector(int[] num) {
+		Random r = new Random();
+		for (int i = 0; i < num.length; i++) {
+			num[i] = r.nextInt(100);
+		}
+		return num;
+	}
+	public  void mostrarVector(int[] num) {
+		System.out.println("Vector Aleatorio: " + Arrays.toString(num));
+	}
 }

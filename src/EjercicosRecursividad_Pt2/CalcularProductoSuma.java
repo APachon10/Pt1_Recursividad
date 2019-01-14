56415a66a18402a065cbe45ec5649698d020c779
APachon10 <Pachondestroy95@gmail.com>
2019-01-14 13:59:45 +0100

    Calcular Producto Suma terminado

--- src/EjercicosRecursividad_Pt2/CalcularProductoSuma.java
+++ src/EjercicosRecursividad_Pt2/CalcularProductoSuma.java
@@ -8,29 +8,28 @@ public class CalcularProductoSuma {
 
 		CalcularProductoSuma c = new CalcularProductoSuma();
 
-		int x[] = new int [10];
-		c.rellenarVector(x);
-		c.mostrarVector(x);
+		int x[] = {1,2,3,4,5,6,7,8,9,10};
+//		int x[] = new int [10];
+//		c.rellenarVector(x);
+//		c.mostrarVector(x);
 		
 		System.out.println("==============");
-		int pos= x.length;
+		int pos= 0;
 		int contador1 = 0;
 		int contador2 = 0;
-		System.out.println("El Resultado és: " + c.calcularProductoRecursivo(x,pos-1,contador1,contador2));
+		System.out.println("El Resultado és: " + calcularProductoRecursivo(x,pos,contador1,contador2));
 	}
 	public static int calcularProductoRecursivo(int [] num, int posicion,int con_m2,int cont_m3 ){
-		if (posicion> num.length) {
+		if (posicion>=num.length) {
 			return con_m2*cont_m3;
 		}
 		if (num[posicion]%2==0) {
-			System.out.println("Multiplo de 2: "+num[posicion]);
 			con_m2 = con_m2 + num[posicion];
-			return calcularProductoRecursivo(num, posicion+1, con_m2, cont_m3);
-		}else if(num[posicion]%3==0) {
-			System.out.println("Multiplo de 3: "+num[posicion]);
-			cont_m3 = cont_m3 + num[posicion];
-			return calcularProductoRecursivo(num, posicion+1, con_m2, cont_m3);
 		}
+		if(num[posicion]%3==0) {
+			cont_m3 = cont_m3 + num[posicion];
+		}
+		return calcularProductoRecursivo(num, posicion+1, con_m2, cont_m3);
 	}
 	public  int [] rellenarVector(int[] num) {
 		Random r = new Random();
